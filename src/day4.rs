5c3376a9b52b5d7c8a43f72dcb5ee1911d34670a
Shantanu Goel <shantanu.goel@gmail.com>
2023-12-04 16:30:59 +0530

    Day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -30,3 +30,35 @@ pub fn part1() {
     }
     println!("Day 4 part 1: {}", sum);
 }
+
+pub fn part2() {
+    let lines = utils::read_lines("./inputs/day4");
+    let re = Regex::new(r"(\d+)").unwrap();
+    let lines_len = lines.len();
+    let mut card_counter: Vec<i32> = vec![1; lines_len];
+    for (index, line) in lines.into_iter().enumerate() {
+        let (_, numbers) = line.split_once(':').unwrap();
+        let (winning_str, my_str) = numbers.split_once('|').unwrap();
+        let winning_numbers: Vec<i32> = re
+            .find_iter(winning_str)
+            .map(|m| m.as_str().parse().unwrap())
+            .collect();
+        let my_numbers: Vec<i32> = re
+            .find_iter(my_str)
+            .map(|m| m.as_str().parse().unwrap())
+            .collect();
+        let mut temp_index = index;
+        for num in my_numbers {
+            if winning_numbers.contains(&num) {
+                temp_index += 1;
+                if temp_index < lines_len {
+                    card_counter[temp_index] += card_counter[index];
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    let sum: i32 = card_counter.iter().sum();
+    println!("Day 4 part 2: {}", sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use advent_of_code_2023::*;
 
 fn main() {
-    day1::part1();
-    day1::part2();
-    day2::part1();
-    day2::part2();
-    day3::part1();
-    day3::part2();
+    // day1::part1();
+    // day1::part2();
+    // day2::part1();
+    // day2::part2();
+    // day3::part1();
+    // day3::part2();
     day4::part1();
+    day4::part2();
 }
