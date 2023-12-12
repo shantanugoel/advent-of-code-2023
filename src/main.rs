4e1b4213734fa04e834b51c194f5e1da9b0d6e92
Shantanu Goel <shantanu.goel@gmail.com>
2023-12-12 11:23:46 +0530

    Day12 init

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ fn main() {
     // day9::part2();
     // day10::part1();
     // day10::part2();
-    day11::part1();
-    day11::part2();
+    // day11::part1();
+    // day11::part2();
+    day12::part1();
+    day12::part2();
 }
