520c1747c43bc052c2111251266e9ebaed8d9858
Shantanu Goel <shantanu.goel@gmail.com>
2023-12-13 18:56:57 +0530

    Day12 Part 2

--- src/day12.rs
+++ src/day12.rs
@@ -12,7 +12,7 @@ lazy_static! {
     };
 }
 
-#[derive(PartialEq, Eq, Clone, Copy, Hash)]
+#[derive(PartialEq, Eq, Clone, Copy, Hash, Debug)]
 enum State {
     Damaged,
     Undamaged,
@@ -30,14 +30,20 @@ fn parse_record(
     let mut record_2_iter = record_2.iter().copied().peekable();
     let mut current_count_in_record_2 = beginning_count;
     let mut not_found = false;
-    // println!(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    let (a, b, c, d) = (
+        record_1.to_string().clone(),
+        record_2.to_vec().clone(),
+        state,                     // state,
+        current_count_in_record_2, // current_count_in_record_2,
+    );
+    let sum3 = *sum;
+    let cache = CACHE.lock().unwrap();
+    if cache.contains_key(&(a.clone(), b.clone(), c, d)) {
+        *sum += cache.get(&(a.clone(), b.clone(), c, d)).unwrap();
+        return;
+    }
+    drop(cache);
     loop {
-        // println!(
-        //     "Parsing record 1: {}, record 2 {:?} current count {}",
-        //     record_1_iter.clone().collect::<String>(),
-        //     record_2_iter.clone().collect::<Vec<i64>>(),
-        //     current_count_in_record_2
-        // );
         match record_1_iter.next() {
             Some('#') => {
                 if current_count_in_record_2 == 0 {
@@ -48,7 +54,6 @@ fn parse_record(
                     if let Some(current_count) = record_2_iter.next() {
                         current_count_in_record_2 = current_count;
                     } else {
-                        // println!("99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999");
                         not_found = true;
                         break;
                     }
@@ -74,40 +79,10 @@ fn parse_record(
                 let mut temp_record_1 = std::iter::once('#')
                     .chain(record_1_iter.clone())
                     .collect::<String>();
-                let temp_record_1_orig = std::iter::once('?')
-                    .chain(record_1_iter.clone())
-                    .collect::<String>();
                 let temp_record_2 = record_2_iter.clone().collect::<Vec<i64>>();
-                let cache = CACHE.lock().unwrap();
-                if cache.contains_key(&(
-                    temp_record_1_orig.clone(),
-                    temp_record_2.clone(),
-                    state,
-                    current_count_in_record_2,
-                )) {
-                    // println!("visited");
-                    // println!("{}", sum);
-                    *sum += cache
-                        .get(&(
-                            temp_record_1_orig.clone(),
-                            temp_record_2.clone(),
-                            state,
-                            current_count_in_record_2,
-                        ))
-                        .unwrap();
-                    // println!("cache get {} {:?}", temp_record_1_orig, temp_record_2);
-                    // println!("{}", sum);
-                    return;
-                }
-                drop(cache);
                 // Can be either . or # at this point.
-                let mut sum2: u64 = *sum;
                 if current_count_in_record_2 != 0 || state != State::Damaged {
                     parse_record(
-                        // std::iter::once('#')
-                        //     .chain(record_1_iter.clone())
-                        //     .collect::<String>()
-                        //     .as_str(),
                         temp_record_1.as_str(),
                         &temp_record_2,
                         state,
@@ -115,34 +90,6 @@ fn parse_record(
                         sum,
                     );
                 }
-                if sum2 < *sum {
-                    let mut cache = CACHE.lock().unwrap();
-                    // println!(
-                    //     "Found a solution: {}, Cache Length {}",
-                    //     *sum - sum2,
-                    //     cache.len()
-                    // );
-                    // println!(
-                    //     "cache put # {} {:?}",
-                    //     temp_record_1_orig.clone(),
-                    //     temp_record_2.clone()
-                    // );
-                    // if cache.len() > 10 {
-                    //     panic!("test");
-                    // }
-                    cache.insert(
-                        (
-                            temp_record_1_orig.clone(),
-                            temp_record_2.clone(),
-                            state,
-                            current_count_in_record_2,
-                        ),
-                        *sum - sum2,
-                    );
-                    drop(cache);
-                }
-                // println!("===== {}", sum);
-                sum2 = *sum;
                 if current_count_in_record_2 == 0 && record_1_iter.peek().is_none() {
                     break;
                 }
@@ -151,10 +98,6 @@ fn parse_record(
                     .collect::<String>();
                 if current_count_in_record_2 == 0 || state == State::Undamaged {
                     parse_record(
-                        // std::iter::once('.')
-                        //     .chain(record_1_iter.clone())
-                        //     .collect::<String>()
-                        //     .as_str(),
                         temp_record_1.as_str(),
                         &temp_record_2,
                         state,
@@ -162,42 +105,6 @@ fn parse_record(
                         sum,
                     );
                 }
-                if sum2 < *sum {
-                    let mut cache = CACHE.lock().unwrap();
-                    // println!(
-                    //     "Found a solution: {}, Cache Length {}",
-                    //     *sum - sum2,
-                    //     cache.len()
-                    // );
-                    // println!(
-                    //     "cache put . {} {:?}",
-                    //     temp_record_1_orig.clone(),
-                    //     temp_record_2.clone()
-                    // );
-                    // if cache.len() > 10 {
-                    //     panic!("test");
-                    // }
-                    // cache.insert(
-                    //     (
-                    //         temp_record_1.clone(),
-                    //         temp_record_2.clone(),
-                    //         state,
-                    //         current_count_in_record_2,
-                    //     ),
-                    //     *sum - sum2,
-                    // );
-                    cache
-                        .entry((
-                            temp_record_1_orig.clone(),
-                            temp_record_2.clone(),
-                            state,
-                            current_count_in_record_2,
-                        ))
-                        .and_modify(|v| *v += *sum - sum2)
-                        .or_insert(*sum - sum2);
-                    drop(cache);
-                }
-                // println!("****** {}", sum);
                 break;
             }
             _ => break,
@@ -210,6 +117,19 @@ fn parse_record(
     {
         *sum += 1;
     }
+    let mut cache = CACHE.lock().unwrap();
+    // println!(
+    //     "cache put default  {} {:?} {:?} {} --> {}",
+    //     a.clone(),
+    //     b.clone(),
+    //     c,
+    //     d,
+    //     *sum - sum3
+    // );
+    cache
+        .entry((a.clone(), b.clone(), c, d))
+        .or_insert(*sum - sum3);
+    drop(cache);
 }
 
 pub fn part1() {
@@ -234,15 +154,6 @@ pub fn part1() {
         parse_record(record_1, record_2, State::Undamaged, 0, &mut sum)
     });
 
-    // let mut x = 0;
-    // for record in records {
-    //     parse_record(
-    //         record.0.as_str(),
-    //         record.1.as_slice(),
-    //         State::Undamaged,
-    //         &mut x,
-    //     );
-    // }
     println!("{} ", sum);
 }
 
@@ -288,62 +199,10 @@ pub fn part2() {
     let (r1, r2) = expand(&records);
     let mut sum = 0;
 
-    // println!("{}", r1[0]);
-    // println!("{:?}", r2[0]);
-    // for i in 0..records.len() {
-    //     parse_record(&r1[i], &r2[i], State::Undamaged, 0, &mut sum);
-    // }
-
-    struct T {
-        s1: String,
-        s2: Vec<i64>,
-    }
-
-    let mut r: Vec<T> = vec![];
-
     for i in 0..records.len() {
-        r.push(T {
-            s1: r1[i].clone(),
-            s2: r2[i].clone(),
-        });
+        parse_record(&r1[i], &r2[i], State::Undamaged, 0, &mut sum);
+        println!("{}", i);
     }
 
-    let result = thread::scope(|s| {
-        let handles: Vec<_> = r
-            .iter()
-            .map(|r| {
-                s.spawn(|_| {
-                    let mut s = 0;
-                    parse_record(&r.s1, &r.s2, State::Undamaged, 0, &mut s);
-                    unsafe {
-                        static mut p: u32 = 0;
-                        p += 1;
-                        println!("{}", p);
-                    }
-                    s
-                })
-            })
-            .collect();
-        for handle in handles {
-            sum += handle.join().unwrap();
-            println!("sum: {}", sum);
-        }
-        sum
-    })
-    .unwrap();
-
-    // records.iter().for_each(|(record_1, record_2)| {
-    //     parse_record(record_1, record_2, State::Undamaged, 0, &mut sum)
-    // });
-
-    // let mut x = 0;
-    // for record in records {
-    //     parse_record(
-    //         record.0.as_str(),
-    //         record.1.as_slice(),
-    //         State::Undamaged,
-    //         &mut x,
-    //     );
-    // }
-    println!("{} ", result);
+    println!("{} ", sum);
 }
