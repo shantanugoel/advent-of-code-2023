70daf6bcbded66712b78c80291896a9a9c35b53a
Shantanu Goel <shantanu.goel@gmail.com>
2023-12-15 15:23:57 +0530

    Day14 Part 2 wip

--- src/day14.rs
+++ src/day14.rs
@@ -21,16 +21,16 @@ pub fn part1() {
     let mut weight = 0;
     let mut spaces: Vec<usize> = vec![];
     for line in lines {
-        println!("{}", line);
+        // println!("{}", line);
         spaces.clear();
         for (i, c) in line.chars().enumerate() {
-            println!("{}: {}", i, c);
+            // println!("{}: {}", i, c);
             match c {
                 'O' => {
                     if spaces.is_empty() {
                         weight += length - i;
                     } else {
-                        println!("Taking up space {}", spaces[0]);
+                        // println!("Taking up space {}", spaces[0]);
                         weight += spaces[0];
                         spaces.remove(0);
                         spaces.push(length - i);
@@ -41,9 +41,89 @@ pub fn part1() {
                 _ => continue,
             }
         }
-        println!("{}: weight {}", line, weight);
+        // println!("{}: weight {}", line, weight);
+    }
+    // println!("weight {}", weight);
+}
+
+#[derive(PartialEq, Eq)]
+enum FlipDirection {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(PartialEq, Eq)]
+enum Direction {
+    North,
+    West,
+    South,
+    East,
+}
+
+fn flip(input: &Vec<String>, direction: FlipDirection) -> Vec<String> {
+    if direction == FlipDirection::Horizontal {
+        input
+            .iter()
+            .map(|line| line.chars().rev().collect())
+            .collect()
+    } else {
+        input.iter().rev().cloned().collect()
+    }
+}
+
+fn tilt(input: &Vec<String>, direction: Direction) -> Vec<String> {
+    match direction {
+        Direction::North => transpose(input),
+        Direction::West => flip(input, FlipDirection::Horizontal),
+        Direction::East => flip(input, FlipDirection::Vertical),
+        Direction::South => transpose(&flip(input, FlipDirection::Horizontal)),
+    }
+}
+
+fn cycle(input: &Vec<String>) -> Vec<String> {
+    let mut output: Vec<String>;
+    output = tilt(input, Direction::North);
+    output = tilt(&output, Direction::West);
+    output = tilt(&output, Direction::South);
+    output = tilt(&output, Direction::East);
+    output
+}
+
+fn compute(input: &Vec<String>) -> usize {
+    let length = input[0].len();
+    let mut weight = 0;
+    let mut spaces: Vec<usize> = vec![];
+    for line in input {
+        // println!("{}", line);
+        spaces.clear();
+        for (i, c) in line.chars().enumerate() {
+            // println!("{}: {}", i, c);
+            match c {
+                'O' => {
+                    if spaces.is_empty() {
+                        weight += length - i;
+                    } else {
+                        // println!("Taking up space {}", spaces[0]);
+                        weight += spaces[0];
+                        spaces.remove(0);
+                        spaces.push(length - i);
+                    }
+                }
+                '.' => spaces.push(length - i),
+                '#' => spaces.clear(),
+                _ => continue,
+            }
+        }
+        // println!("{}: weight {}", line, weight);
+    }
+    weight
+}
+pub fn part2() {
+    let lines = get_input("./inputs/day14");
+    let mut weight = 0;
+    for i in 0..1000000000 {
+        println!("{}: {}", i, weight);
+        weight = compute(&cycle(&lines));
     }
     println!("weight {}", weight);
 }
-
-pub fn part2() {}
