90230d6186f0ebd5070e1b194d9bc0c8d10c477b
Shantanu Goel <shantanu.goel@gmail.com>
2023-12-16 18:18:37 +0530

    Day15 Part2

--- src/day15.rs
+++ src/day15.rs
@@ -19,4 +19,45 @@ pub fn part1() {
     println!("{}", compute(input[0].as_str()));
 }
 
-pub fn part2() {}
+#[derive(Clone)]
+struct Lens {
+    label: String,
+    focus: u32,
+}
+
+pub fn part2() {
+    let input = utils::read_lines("./inputs/day15");
+    let mut boxes: Vec<Vec<Lens>> = vec![Vec::new(); 256];
+
+    for lens in input[0].split(',') {
+        if let Some((label, focus)) = lens.split_once('=') {
+            let mut found = false;
+            let hash = hash(label, 0);
+            for lens in boxes[hash as usize].iter_mut() {
+                if lens.label == label {
+                    lens.focus = focus.parse().unwrap();
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                boxes[hash as usize].push(Lens {
+                    label: label.to_string(),
+                    focus: focus.parse().unwrap(),
+                });
+            }
+        } else {
+            let label = &lens[..lens.len() - 1];
+            let hash = hash(label, 0);
+            boxes[hash as usize].retain(|lens| lens.label != label);
+        }
+    }
+
+    let mut power = 0;
+    for (index, boxx) in boxes.iter().enumerate() {
+        for (slot, lens) in boxx.iter().enumerate() {
+            power += (index as u32 + 1) * (slot as u32 + 1) * lens.focus;
+        }
+    }
+    println!("{}", power);
+}
